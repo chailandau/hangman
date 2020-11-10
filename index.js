4d5378498ad847e38500bde64723ec6d34a9e0de
Chai <chai.landau@gmail.com>
2020-11-10 16:30:28 -0500

    setting up index

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const wordBank = require("./word-bank.json");
 const prompt = require("readline-sync");
 
-const answer = prompt.question("May I have your name?");
+// Game round counter
+let gameRounds = 0;
+let gameRoundsCounter = (gameRounds) => {
+  gameRounds +=1;
+}
 
-console.log(answer);
+// Guess counter
+let guesses = 0;
+let guessCounter = (guesses) => {
+  guesses += 1;
+}
+
+// Choose random word
+let random = Math.floor(Math.random() * wordBank.length);
+let randomWord = wordBank[random];
+
+// Build guessed word
+let guessedWord = "";
+
+
+// Game mechanics
+const hangmanGame = (gameRounds) => {
+  guesses;
+  const chosenWord = randomWord;
+  const gameMechanics = (guess) => {
+    guessCounter();
+
+    if (guessedWord === chosenWord) {
+      return "You got it!"
+    }
+  }
+}
+
+const letter = prompt.question("Please guess a letter: ");
